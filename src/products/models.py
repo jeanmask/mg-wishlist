a9b74d5ae3729c11646710095c8081e494ed2957
Jean Rodrigues <misc@jean.bz>
2021-07-20 19:04:42 -0300

    Create app products and model Products to cache challenge-api results

---
+++ src/products/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.db.models.fields import (
+    CharField,
+    DateTimeField,
+    DecimalField,
+    URLField,
+    UUIDField,
+)
+
+
+class Product(models.Model):
+    id = UUIDField(primary_key=True)
+
+    title = CharField(max_length=255)
+    image = URLField(blank=True)
+    price = DecimalField(max_digits=10, decimal_places=2)
+    review_score = DecimalField(max_digits=7, decimal_places=6, null=True)
+
+    last_update = DateTimeField(auto_now=True, db_index=True)
