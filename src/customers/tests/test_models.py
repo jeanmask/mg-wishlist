19e9b3279233eb44356ecf2247a8eb8ed86c9604
Jean Rodrigues <misc@jean.bz>
2021-07-20 17:33:52 -0300

    Create test to validate unique email and non-null fields

--- src/customers/tests/test_models.py
+++ src/customers/tests/test_models.py
@@ -1,6 +1,6 @@
 import pytest
 from django.core.validators import validate_email
-
+from django.db.utils import IntegrityError
 from customers.models import Customer
 
 
@@ -9,4 +9,18 @@ def test_customer_create(customer):
     obj = Customer.objects.create(**customer)
     assert obj.name == customer["name"]
     assert obj.email == customer["email"]
-    assert validate_email(obj.email) is None, "Customer email must be a valid email"
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize("field", ("name", "email"))
+def test_customer_required_fields(customer, field):
+    with pytest.raises(IntegrityError, match=f"customers_customer.{field}"):
+        customer[field] = None
+        Customer.objects.create(**customer)
+
+
+@pytest.mark.django_db
+def test_customer_unique_email(customer):
+    Customer.objects.get_or_create(email=customer["email"], defaults=customer)
+    with pytest.raises(IntegrityError, match="customers_customer.email"):
+        Customer.objects.create(**customer)
