34cdcfa9574abe42e4c85cb01d5544c6b94daa6f
Jean Rodrigues <misc@jean.bz>
2021-07-22 18:57:26 -0300

    Rename project to mg_wishlist

--- src/mg_wishlist/asgi.py
+++ src/mg_wishlist/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mg_ticket.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mg_wishlist.settings")
 
 application = get_asgi_application()
--- src/mg_wishlist/wsgi.py
+++ src/mg_wishlist/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for mg_ticket project.
+WSGI config for mg_wishlist project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mg_ticket.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mg_wishlist.settings")
 
 application = get_wsgi_application()
