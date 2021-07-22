de7bfb4157c9ca91c93c3f91037daa18e0c6c18c
Jean Rodrigues <misc@jean.bz>
2021-07-22 14:54:20 -0300

    Add CustomerManager on Customer model

--- src/customers/models.py
+++ src/customers/models.py
@@ -3,11 +3,15 @@ import uuid
 from django.db import models
 from django.db.models.fields import CharField, EmailField, UUIDField
 
+from .managers import CustomerManager
+
 
 class Customer(models.Model):
     id = UUIDField(primary_key=True, default=uuid.uuid4, editable=False, help_text="Customer UUID")
     name = CharField(blank=False, max_length=255, help_text="Customer name")
     email = EmailField(blank=False, unique=True, help_text="Customer unique email address")
+
+    objects = CustomerManager()
 
     def __repr__(self):
         return f"{self.name} <{self.email}>"
