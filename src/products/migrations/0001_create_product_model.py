e6fbe4b5c7bf7bffdf5464ae2a9ce4fe185798a6
Jean Rodrigues <misc@jean.bz>
2021-07-23 11:41:23 -0300

    Add brand and update title max_length to 254 on Product model

--- src/products/migrations/0001_create_product_model.py
+++ src/products/migrations/0001_create_product_model.py
@@ -14,8 +14,9 @@ class Migration(migrations.Migration):
             name="Product",
             fields=[
                 ("id", models.UUIDField(primary_key=True, serialize=False)),
-                ("title", models.CharField(max_length=255)),
+                ("title", models.CharField(max_length=254)),
                 ("image", models.URLField(blank=True)),
+                ("brand", models.CharField(max_length=254)),
                 ("price", models.DecimalField(decimal_places=2, max_digits=10)),
                 (
                     "review_score",
--- src/products/models.py
+++ src/products/models.py
@@ -5,8 +5,9 @@ from django.db.models.fields import CharField, DateTimeField, DecimalField, URLF
 class Product(models.Model):
     id = UUIDField(primary_key=True)
 
-    title = CharField(max_length=255)
+    title = CharField(max_length=254)
     image = URLField(blank=True)
+    brand = CharField(max_length=254)
     price = DecimalField(max_digits=10, decimal_places=2)
     review_score = DecimalField(max_digits=7, decimal_places=6, null=True)
 
